c47e3c995b0506c16bd0d22b59c1fa6c4e97d865
Mark Murnane <mark@hackafe.net>
2018-05-17 02:09:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 import sys
 from setuptools import setup, find_packages
 
-def read_license():
-    with open("LICENSE") as f:
-        return f.read()
-
 setup(
     name='dmx485',
     packages=find_packages(),
@@ -12,7 +8,7 @@ setup(
     description='Simple DMX driver using an RS485 adapter',
     long_description="""Simple DMX driver using an RS485 adapter
     """,
-    license=read_license(),
+    license='MIT',
     author='Mark Murnane',
     author_email='mark@hackafe.net',
     url='https://github.com/bitbyt3r/dmx',
